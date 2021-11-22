107ffe6723bb5093b168d3304e11edf18810bf15
Nishanth <innocentnishanth@gmail.com>
2021-11-22 21:30:52 +0530

    char counter

---
+++ python/countChar.py
@@ -0,0 +1,14 @@
+# Python 3.x.x
+sentence = input("Enter the sentence : ")
+# Python 2.7.x
+# sentence = raw_input("Enter the sentence : ")
+
+count = {}
+for letter in sentence:
+    if letter in count:
+        count[letter] += 1
+    else:
+        count[letter] = 1
+
+print("Count of all characters in "+sentence + " is :\n "
+      + str(count))
