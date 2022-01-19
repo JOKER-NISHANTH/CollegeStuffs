7f33da16403b34d2028c2f0307a4ff599fde9f61
Nishanth <innocentnishanth@gmail.com>
2022-01-19 19:03:46 +0530

    Occur added

---
+++ PerlPython-Lab/Python/occurs.py
@@ -0,0 +1,9 @@
+a = "Nishanth Nishanth"
+c = 0
+for i in a:
+    if i == 'a':
+        c += 1
+print(f"The occurrence of character in the a is {c}")
+
+# OUTPUT
+# The occurrence of character in the a is 2
