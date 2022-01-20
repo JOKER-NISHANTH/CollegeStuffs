fa5175e444afaefa3e1f6b90a9abfe9bbc41b578
Nishanth <innocentnishanth@gmail.com>
2022-01-20 21:15:45 +0530

    bank updated

---
+++ PerlPython-Lab/Python/bank.py
@@ -0,0 +1,35 @@
+
+class Bank:
+
+    def __init__(self, name="", balance=0.0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withDraw(self, amount):
+        if amount > self.balance:
+            print("Balance amount is less , so no withdrawal.")
+        else:
+            self.balance -= amount
+        return self.balance
+
+
+name = input("Enter the name : ")
+b = Bank(name)
+
+while(True):
+    print(' d -Desposit , w -Withdraw , e -Exit')
+    choice = input("Enter your choice : ").lower()
+
+    if choice == 'e':
+        exit()
+    amt = float(input("Enter the amount : "))
+
+    if choice == 'd':
+        print("Balance after deposit : ", b.deposit(amt))
+
+    elif choice == 'w':
+        print("Balance after withdraw : ", b.withDraw(amt))
