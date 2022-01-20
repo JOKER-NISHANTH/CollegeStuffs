a463b9718c734a145e35ffa6e6ab2aa8d41c7759
Nishanth <innocentnishanth@gmail.com>
2022-01-20 19:56:24 +0530

    towerOfHanoi Updated

--- PerlPython-Lab/Python/towerOfHanoi.py
+++ PerlPython-Lab/Python/towerOfHanoi.py
@@ -1,26 +1,28 @@
-def towers(n, a, c, b):
-    if n == 1:
-        print("Move disk %i from pole %s to pole %s" % (n, a, c))
-    else:
-        towers(n-1, a, b, c)
-        print("Move disk %i from pole %s to pole %s" % (n, a, c))
-        towers(n-1, b, c, a)
+
+def towers(n, a, b, c):
+    if(n == 1):
+        return print('Move disk %d from pole %s to pole %s. ' % (n, a, c))
+
+    towers(n - 1, a, c, b)
+    print('Move disk %d from pole %s to pole %s. ' % (n, a, c))
+    towers(n - 1, b, a, c)
 
 
-n = int(input("Enter the number of disk : "))
+n = int(input('Enter the number of disks: '))
+
 towers(n, 'A', 'B', 'C')
+
 
 '''
             OUTPUT
 
 Enter the number of disk : 3
-Move disk 1 from pole A to pole B
-Move disk 2 from pole A to pole C
-Move disk 1 from pole B to pole C
-Move disk 3 from pole A to pole B
-Move disk 1 from pole C to pole A
-Move disk 2 from pole C to pole B
-Move disk 1 from pole A to pole B
-
+Move disk 1 from pole A to pole C.
+Move disk 2 from pole A to pole B.
+Move disk 1 from pole C to pole B.
+Move disk 3 from pole A to pole C.
+Move disk 1 from pole B to pole A.
+Move disk 2 from pole B to pole C.
+Move disk 1 from pole A to pole C.
 
 '''
